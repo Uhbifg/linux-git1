d1d3aa159ca569662c174199fa8a63a37674a8ce
Uhbifg <gri279@gmail.com>
2020-12-13 04:20:39 +0300

    re

--- project2/code/main.cpp
+++ project2/code/main.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h> 
 
-int main(int argc, char *argv[]){
-    char *sal = "World";
-    if (argc>1){
-        sal = argv[1];
-    }
-    printf("Hello, %s!\n",sal);
+int main(){
+    printf("Hello, %s!\n");
     return 0;
 }
